e4ef683812bf84c9290f64c2c910a07da2a0437f
alexlungu10 <alexlungu10@users.noreply.github.com>
2019-06-25 12:44:12 +0300

    update type script

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,32 +1,53 @@
 import * as React from "react";
-export default  class Person extends React.Component{
+//import Any = jasmine.Any;
+
+export default class Person extends React.Component<Props,State> {
+
+    constructor(props:Props) {
+        super(props);
+        this.changeVin = this.changeVin.bind(this);
+        this.state={vin:'1234'}
+    }
+
+    changeVin(e:any){
+    this.setState({vin:e.target.value})
+    }
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-        const age=22;
-        const vin=1234;
+        const age = 22;
 
         Person.getName();
         return (
-            <div> <p>My name is {Person.getName()}, I'm {age}  </p>
+            <div><p>My name is {Person.getName()}, I'm {age}  </p>
 
 
                 <form>
-                    <input className="input-el" type="text" value={vin}
-                           placeholder="insert VIN" />
+                    <input value={this.state.vin} className="input-el" type="text" placeholder="insert VIN" max={5}
+                           onChange={this.changeVin} maxLength={5}></input>
+                    <button >Submit</button>
                 </form>
                 <br/>
-                <p>Vin is: {vin}</p>
+                <p>Vin is: {this.state.vin}</p>
 
-                <input value={vin} className="input-el" type="text" placeholder="insert VIN"  max={5}></input>
-                <button value={vin} >Submit</button>
+
 
             </div>
         );
 
     }
 
-    static getName():string {
+    static getName(): string {
         return "andy";
     }
 
+
+
+}
+
+interface Props {
+//... // your props validation
+}
+
+interface State {
+  vin:string
 }
